8bc734665c5d5c568b96f876ba22080819ae3718
Kirill Cherkashin <kirill.cherkashin@lab49.com>
2014-07-02 00:42:21 -0400

    Introduce transforms module to perform board mirroring/flipping/ rotating

---
+++ src/tools/transforms.js
@@ -0,0 +1,98 @@
+var utils = require('./utils');
+/**
+ * This is a rather naive implementation of board mirroring flipping and rotations.
+ *
+ * At some point I am planning to spend some time making it more performant
+ * @param array
+ * @returns position
+ */
+function arrayReverse(array) {
+    return array.reverse();
+}
+
+
+function transform(position, callback) {
+    return position.map(function (line, i) {
+        return line.map(function (cell, j) {
+            return callback(position, i, j);
+        });
+    })
+}
+
+
+module.exports = {
+    /**
+     * Mirrors position horizontally
+     *
+     * @param position
+     * @returns position
+     */
+    horizontal: function (position) {
+        return utils.clonePosition(position).map(arrayReverse);
+    },
+
+    /**
+     * Mirrors position vertically
+     *
+     * @param position
+     * @returns position
+     */
+    vertical: function (position) {
+        return utils.clonePosition(position).reverse();
+    },
+
+    /**
+     * Rotate position clockwise
+     *
+     * @param position
+     * @returns position
+     */
+    clockwise: function (position) {
+        return transform(position, function (position, i, j) {
+            return position[position.length - 1 - j][i];
+        });
+    },
+
+
+    /**
+     * Rotate position counter clockwise
+     *
+     *
+     * @param position
+     * @returns position
+     */
+    counterClockwise: function (position) {
+        return transform(position, function (position, i, j) {
+            return position[ j][position.length - 1 - i];
+        });
+    },
+
+
+    /**
+     * Flips position diagonally from top left to bottom right corner
+     *
+     *
+     * @param position
+     * @returns position
+     */
+    diagonalFromLeftTopToRightBottom: function (position) {
+        return transform(position, function (position, i, j) {
+            return position[ position.length - 1 - j][position.length - 1 - i];
+        });
+    },
+
+
+    /**
+     * Flips position diagonally from top right to bottom left corner
+     *
+     *
+     * @param position
+     * @returns position
+     */
+    diagonalFromRightTopToLeftBottom: function (position) {
+        return transform(position, function (position, i, j) {
+            return position[j][i];
+        });
+    }
+
+};
---
+++ test/tools/transformsTest.js
@@ -0,0 +1,81 @@
+var Game = require('../../src/index.js').Game;
+var transform = require('../../src/tools/transforms');
+var utils = require('../../src/tools/utils');
+exports.setUp = function (done) {
+    this.game = new Game({
+        strategy: 'ticTacToe'
+    });
+    this.game.moveTo('b2', 'a1', 'a3', 'c1', 'b1');
+    done();
+};
+exports.testRotateHorizontal = function (test) {
+    var original = [
+        [0, 0, 1],
+        [0, 1, 0],
+        [2, 1, 2]
+    ];
+    var position = this.game.getPosition();
+    console.log(position);
+
+    test.deepEqual(transform.horizontal(this.game.getPosition()), original);
+    console.log(position);
+    test.done();
+};
+exports.testRotateHorizontalDoesNotMutateTheOriginal = function (test) {
+    var position = this.game.getPosition();
+    var original = this.game.getPosition();
+    transform.horizontal(position);
+    test.deepEqual(position, original);
+    test.done();
+};
+
+exports.testRotateVertical = function (test) {
+    test.deepEqual(transform.vertical(this.game.getPosition()), [
+        [2, 1, 2],
+        [0, 1, 0],
+        [1, 0, 0]
+    ]);
+    test.done();
+};
+
+exports.testRotateClockwise = function (test) {
+    test.deepEqual(transform.clockwise(this.game.getPosition()), [
+        [2, 0, 1],
+        [1, 1, 0],
+        [2, 0, 0]
+    ]);
+    test.done();
+};
+
+exports.testRotateCounterClockwise = function (test) {
+    test.deepEqual(transform.counterClockwise(this.game.getPosition()), [
+        [0, 0, 2],
+        [0, 1, 1],
+        [1, 0, 2]
+    ]);
+    test.done();
+};
+exports.testTwiceClockwisIsTheSameTwiceCounterClockwise = function (test) {
+    var clockwise = transform.clockwise(transform.clockwise(this.game.getPosition()));
+    var counterclockwise = transform.counterClockwise(transform.counterClockwise(this.game.getPosition()));
+    test.deepEqual(clockwise, counterclockwise);
+    test.done();
+};
+
+exports.testDiagonalFromLeftTopToRightBottom = function (test) {
+    test.deepEqual(transform.diagonalFromLeftTopToRightBottom(this.game.getPosition()), [
+        [2, 0, 0],
+        [1, 1, 0],
+        [2, 0, 1]
+    ]);
+    test.done();
+};
+
+exports.testDiagonalFromRightTopToLeftBottom = function (test) {
+    test.deepEqual(transform.diagonalFromRightTopToLeftBottom(this.game.getPosition()), [
+        [1, 0, 2],
+        [0, 1, 1],
+        [0, 0, 2]
+    ]);
+    test.done();
+};
