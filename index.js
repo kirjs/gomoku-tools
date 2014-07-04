685bb3999479f6b167c303fb539b94e183180c23
Kirill Cherkashin <kirill.cherkashin@lab49.com>
2014-07-03 22:42:59 -0400

    Add simple interface that allows to create game instances

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./src/index.js');
+var Game = require('./src/index.js');
+
+module.exports = {
+    Gomoku: function (config) {
+        config = config || {};
+        config.strategy = 'gomoku';
+        return new Game(config);
+    },
+    TicTacToe: function (config) {
+        config = config || {};
+        config.strategy = 'ticTacToe';
+        return new Game(config);
+    },
+    Renju: function (config) {
+        config = config || {};
+        config.strategy = 'renju';
+        return new Game(config);
+    }
+};
