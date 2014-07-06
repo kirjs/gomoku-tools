b4f2317b2c2f8cdf6c6c8d67f1289354d0a0cf23
Kirill Cherkashin <kirill.cherkashin@lab49.com>
2014-07-05 21:38:03 -0400

    Add publish task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var rimraf = require('gulp-rimraf');
-
+var bump = require('gulp-bump');
+var git = require('gulp-git');
+function log(e) {
+    console.log(e);
+}
 
 gulp.task('build', function () {
     gulp.src('src/index.js')
@@ -15,12 +19,29 @@ gulp.task('build', function () {
         .pipe(gulp.dest('./build/'));
 });
 
+gulp.task('publish', function () {
+    gulp.src('./build')
+        .pipe(git.add())
+        .on('error', console.log)
+        .pipe(git.commit('Add the latest built version'))
+        .on('error', console.log)
+        .pipe(git.push())
+
+        .on('error', console.log);
+});
+
+gulp.task('bump', function () {
+    gulp.src(['package.json', 'bower.json'])
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
+});
+
 
 gulp.task('clean', function (cb) {
     rimraf('./build', cb);
 });
 
 
-gulp.task('default', ['clean', 'build']);
+gulp.task('default', ['clean', 'build', 'publish', 'bump']);
 
 
